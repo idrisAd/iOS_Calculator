8ed63ab2b61f39ca4130987636fd1d4f478f69d2
Unknown <idrisadrien@hotmail.fr>
2017-11-22 11:06:38 +0100

    test debug/release

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,14 +12,11 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var opeLabel: UILabel!
-    
     var num1: Int = 0
     var num2: Int = 0
-    
     @IBAction func didPressButton(_ sender: Any) {
         guard let button = sender as? UIButton else {
             print("USE ONLY WITH NUMBERS")
-            
             return
         }
         guard let labeltext = resultLabel.text, let buttonText = button.titleLabel?.text else {
@@ -27,30 +24,22 @@ class ViewController: UIViewController {
             return
         }
         self.resultLabel.text = labeltext + buttonText
-        
             }
-    
-    
-    
     // Action on operation button
     @IBAction func didPressOperation(_ sender: Any) {
-        guard let button = sender as? UIButton else{
-            return
+        guard let button = sender as? UIButton
+            else {
+                return
         }
         opeLabel.text = button.titleLabel?.text
         self.num1 = Int(resultLabel.text!) ?? 0
         resultLabel.text = "0"
-        
     }
-    
-    
     // Action on equal button
     @IBAction func opAction(_ sender: Any) {
-        
         guard let operation: String = opeLabel.text else {
             return
         }
-        
         switch operation {
         case "+":
             self.num2 = Int(resultLabel.text!)! + num1
@@ -68,19 +57,20 @@ class ViewController: UIViewController {
             return
         }
     }
-    
-    
-    
     // Action on Clear button
     @IBAction func didPressClear(_ sender: Any) {
         resultLabel.text = "0"
     }
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        super.view.backgroundColor = .black
+        
+        let env = Bundle.main.infoDictionary!["EqualColor"] as! String
+        if env == "yellow" {
+            super.view.backgroundColor = .black
+        }else{
+            super.view.backgroundColor = .lightGray
+        }
         self.resultLabel.text = "0"
     }
 
@@ -88,24 +78,21 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         self.resultLabel.alpha = 0
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        UIView.animate(withDuration: 2) { 
+        UIView.animate(withDuration: 2) {
             self.resultLabel.alpha = 1
         }
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         super.view.backgroundColor = .yellow
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+
+target 'Calculator' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Sentry', :git => 'https://github.com/getsentry/sentry-cocoa.git', :tag => '3.9.1'
+
+  # Pods for Calculator
+
+  target 'CalculatorTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CalculatorUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
